1acb2075c2de8a2bed9a35cd32bd3f7e7122b823
jhunderdog <33382water@gmail.com>
2022-01-18 20:12:47 +0900

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by jhunderdog on 2022/01/18.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
