3849169c3b6ffa06c636be0ba3af228fc796eafa
jhunderdog <33382water@gmail.com>
2022-01-22 10:23:51 +0900

    mapkit

---
+++ Africa/View/ExternalWeblinkView.swift
@@ -0,0 +1,36 @@
+//
+//  ExternalWeblinkView.swift
+//  Africa
+//
+//  Created by jhunderdog on 2022/01/22.
+//
+
+import SwiftUI
+
+struct ExternalWeblinkView: View {
+    
+    let animal: Animal
+    
+    var body: some View {
+        GroupBox {
+            HStack {
+                Image(systemName: "globe")
+                Text("Wekipedia")
+                Spacer()
+                Group {
+                    Image(systemName: "arrow.up.right.square")
+                    Link(animal.name, destination: (URL(string: animal.link) ?? URL(string: "https://wikipedia.org"))!)
+                }.foregroundColor(.accentColor)
+            } //: HSTACK
+        } // : BOX
+    }
+}
+
+struct ExternalWeblinkView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        ExternalWeblinkView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
